27e3829e17ebea53c1929fd4acec5a2b7c232437
nathan-zucker <114187880+nathan-zucker@users.noreply.github.com>
2023-05-30 13:15:29 -0400

    setting up layout wip

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
+
+import Nav from './components/Nav';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.topPadding}></View>
+      <Nav />
+      <Button
+        onPress={() => {
+          console.log('You tapped the button!');
+        }}
+        title="Press Me"
+      />
       <Text>Welcome to the SOAP note app!</Text>
       <SafeAreaView>
         <Text>Here is the safe area view.</Text>
@@ -16,8 +26,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'top',
+    backgroundColor: '#cdcdcd'
+  },
+  topPadding: {
+    height: '3%',
+    width: '100%'
+  },
+  navbar: {
+    color: 'whitesmoke',
+    backgroundColor: '#1a1a1a',
+    width: '100%',
+    textAlign: 'center',
+    height: '12.5%',
+    fontSize: 25,
+    display: 'flex',
     justifyContent: 'center',
+    padding: '5%'
   },
 });
---
+++ components/Nav.js
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, View, SafeAreaView, Button, Alert, Pressable } from 'react-native';
+
+export default function Nav() {
+    return (
+        <View 
+            style={styles.container}
+        >
+            <Pressable
+            style={styles.button}
+            onPress={()=>{
+                console.log('checkup')
+                Alert.alert('checkup!')
+            }}
+            >
+                <Text style={{fontSize: 22}}>checkup</Text>
+            </Pressable>
+            <Pressable
+                style={Object.assign({}, styles.button, {
+                    backgroundColor: 'limegreen'
+                })}
+                onPress={()=>{
+                    console.log('new patient')
+                    Alert.alert('new patient!')
+                }}
+            >
+                <Text style={{fontSize: 22, fontWeight: 500}}>New SOAP</Text>
+            </Pressable>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '12.5%',
+        backgroundColor: '#1a1a1a',
+        flexDirection: 'row',
+        justifyContent: "space-around",
+        padding: '5%'
+    },
+    button: {
+        backgroundColor: 'white',
+        width: '40%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: '2.5%'
+    }
+})
