a6486064f38d867b1be3f6e0a8a3d70ef82c246d
nathan-zucker <114187880+nathan-zucker@users.noreply.github.com>
2023-08-04 12:58:01 -0400

    wip

--- features/cameraSlice.js
+++ features/cameraSlice.js
@@ -6,11 +6,7 @@ export const cameraSlice = createSlice({
     name: 'camera',
     initialState: {
         active: false,
-        photos: [
-            {
-                source: logo
-            }
-        ],
+        photos: [],
     },
     reducers: {
         toggleCamera: (state) => {
